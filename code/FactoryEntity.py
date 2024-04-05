4bb115df8f911e6df21aea4a90083bddda797ebd
marcio.antonio <marcio.antonio.ferreira@escola.pr.gov.br>
2024-04-05 16:56:38 -0300

    menu Text

---
+++ code/FactoryEntity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class FactoryEntity:
+    def __init__(self):
+        pass
+
+    def getEntity(self, ):
+        pass
+
+    def get_entity(self, entity_type):
+        pass
+
